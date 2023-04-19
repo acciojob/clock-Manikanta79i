d3d45d17dae6cb709379a2bdddea8580533e9691
Manikanta Itikela <113256332+Manikanta79i@users.noreply.github.com>
2023-04-19 12:55:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+    function showTime() {
+        const date = new Date();
+        const options = { 
+          year: 'numeric', 
+          month: 'numeric', 
+          day: 'numeric',
+          hour: 'numeric', 
+          minute: 'numeric', 
+          second: 'numeric',
+          hour12: true
+        };
+        const indianTime = new Intl.DateTimeFormat('en-US', options).format(date);
+        document.getElementById('timer').innerHTML = indianTime;
+      }
+      setInterval(showTime, 1000);
